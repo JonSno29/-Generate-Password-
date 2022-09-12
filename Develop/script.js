25ae04c633d20d228e3b6f0dc07c8e9b330e2c66
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-09-12 11:36:03 -0700

    variables and if

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,25 @@
 // Assignment code here
-
+var specialChar = ["!","@","#","$","%","&"]
+var lowerCase = ["a","b"]
+var upperCase = ["A"]
+var numbers = ["1"]
+function generatePassword() {
+ var lengthofpass = prompt ("password must be 8-128 characters")
+ console.log (lengthofpass)
+ if (lengthofpass <=128 && lengthofpass >=8){
+  console.log ("passwordcheck")
+  var confirmNumber = confirm("do you want numbers")
+  console.log (confirmNumber)
+  var confirmLower = confirm ("do you want lower case letters")
+  if (confirmNumber){ 
+    
+  }
+ } else {
+ generatePassword()
+ }
+  return "hello"
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
