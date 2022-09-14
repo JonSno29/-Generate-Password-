ac8f8b9ed07a8bda3fad9479dbd791096b2a9f30
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-09-14 11:32:45 -0700

    Changed css and html back to original

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];var 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 function generatePassword() {
- var lengthofpass = prompt ("password must be 8-128 characters")
+ var lengthofpass = prompt ("Password must be 8-128 characters")
  console.log (lengthofpass)
  if (lengthofpass <=128 && lengthofpass >=8){
   console.log ("passwordcheck")
-  var confirmNumber = confirm("do you want numbers")
+  var confirmNumber = confirm("Do you want numbers?")
   console.log (confirmNumber)
-  var confirmLower = confirm ("do you want lower case letters")
+  var confirmLower = confirm("Do you want lower case letters?")
   if (confirmNumber){ 
     
   }
  } else {
  generatePassword()
  }
-  return "hello"
+  return "Generated password will go here!";
   
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
